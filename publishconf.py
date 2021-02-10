a34d11d2773728f4e3411734dbf2cff69ededb64
NoFishLikeIan <andreatitton96@gmail.com>
2021-02-10 08:48:55 +0100

    🐛 fixed build bugs

--- publishconf.py
+++ publishconf.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import sys
+import os
+sys.path.append('.')
+
+from pelicanconf import *
 
 # This file is only used if you use `make publish` or
 # explicitly specify it as your config file.
 
-from pelicanconf import *
-import os
-import sys
-sys.path.append(os.curdir)
-
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'nofishlikeian.github.io'
+SITEURL = 'https://nofishlikeian.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
