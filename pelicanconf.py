7f54fb15d2eea572b429f32118c8234288503949
NoFishLikeIan <andreatitton96@gmail.com>
2022-06-30 12:32:43 +0200

    🦉 added favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -55,4 +55,7 @@ DISPLAY_PAGES_ON_MENU = True
 
 PLUGIN_PATHS = ['./activate_plugins']
 PLUGINS = ['render_math']
-STATIC_PATHS = ['images']
+STATIC_PATHS = ['images', 'extra']
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
