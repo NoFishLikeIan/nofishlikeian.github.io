4fcefc4f4ad7105c44afa35580f6a8836de933b7
NoFishLikeIan <andreatitton96@gmail.com>
2024-10-22 16:25:55 +0200

    🦉 jmp update

--- scripts/update-jmp.sh
+++ scripts/update-jmp.sh
@@ -1,3 +1,7 @@
 rm static/docs/tipping-point.pdf
 
 cp $HOME/economics/scc-tipping-points/papers/job-market-paper/social-planner/main.pdf static/docs/tipping-point.pdf
+
+git add .
+git commit -m "🦉 jmp update"
+git push
